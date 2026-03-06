1340ec5367ebc72e2381d13c8295f63586846fcf
Melissa.Karp <Melissa.Karp@noaa.gov>
2026-03-06 14:30:10 -0500

    setting up project repo

---
+++ Rscript/Simulated_Ecosystem_OM.R
@@ -0,0 +1,12 @@
+#This script creates the simulated ecosystem operating model and gets the "true" values from it
+
+#### ---------Setup, load packages ####
+
+#### ----------Initializing input files and directories ####
+
+#### ---------- Get Truth .... ####
+
+#### ---------- Generate "Data" from OM for testing EMs ####
+ ## Create survey
+
+ ## Create catch
