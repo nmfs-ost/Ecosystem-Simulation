dcf704020e72c46edb9040b39c200d893507339f
Bai-Li-NOAA <bai.li@noaa.gov>
2026-06-02 13:44:37 +0000

    fix(R scripts): update to latest ecosystemom functions
    - address issue #2 to use latest ecosystemom functions
    - add code to prepare data and parameters for FIMS runs

--- Rscript/plot_data.R
+++ Rscript/plot_data.R
@@ -1,4 +1,4 @@
-plot_index_comparison <- function(df, title = "Index Comparison") {
+plot_index_comparison <- function(df, file_path, title = "Index Comparison") {
   # Prepare data for ggplot (gathering Truth and Sampled into one column)
   plot_df <- df |>
     dplyr::select(truth_year, truth_value, sampled_value) |>
@@ -17,7 +17,7 @@ plot_index_comparison <- function(df, title = "Index Comparison") {
 
   ggplot2::ggsave(
     filename = paste0(title, ".png"),
-    path = file.path("figures"),
+    path = file_path,
     plot = p,
     width = 8,
     height = 6,
@@ -26,7 +26,7 @@ plot_index_comparison <- function(df, title = "Index Comparison") {
 
 }
 
-plot_age_comp_normalized <- function(df, title) {
+plot_age_comp_normalized <- function(df, title, file_path) {
 
   plot_df <- df |>
     dplyr::group_by(truth_year) |>
@@ -62,7 +62,7 @@ plot_age_comp_normalized <- function(df, title) {
 
   ggplot2::ggsave(
     filename = paste0(title, ".png"),
-    path = file.path("figures"),
+    path = file_path,
     plot = p,
     width = 8,
     height = 6,
@@ -70,7 +70,7 @@ plot_age_comp_normalized <- function(df, title) {
   )
 }
 
-plot_weight_trends <- function(df) {
+plot_weight_trends <- function(df, file_path) {
   p <- ggplot2::ggplot(df, ggplot2::aes(x = truth_year, y = truth_value, color = truth_group)) +
     ggplot2::geom_line(linewidth = 1) +
     ggplot2::theme_minimal() +
@@ -78,7 +78,7 @@ plot_weight_trends <- function(df) {
 
   ggplot2::ggsave(
     filename = "weight_at_age_om.png",
-    path = file.path("figures"),
+    path = file_path,
     plot = p,
     width = 8,
     height = 6,
@@ -86,7 +86,7 @@ plot_weight_trends <- function(df) {
   )
 }
 
-plot_survey_vs_catch <- function(survey_df, catch_df) {
+plot_survey_vs_catch <- function(survey_df, catch_df, file_path) {
   # Normalize both to their mean so they are on the same scale
   s <- survey_df |>
     dplyr::mutate(Value = sampled_value / mean(sampled_value), Source = "Survey")
@@ -101,14 +101,14 @@ plot_survey_vs_catch <- function(survey_df, catch_df) {
   p <- ggplot2::ggplot(
     combined, ggplot2::aes(x = truth_year, y = Value, color = Source)
   ) +
-    ggplot2::geom_line(size = 1) +
+    ggplot2::geom_line(linewidth = 1) +
     ggplot2::theme_minimal() +
     ggplot2::labs(title = "Normalized Survey vs. Catch Indices",
                   x = "Year", y = "Relative Index")
 
   ggplot2::ggsave(
     filename = "Normalized Survey vs. Catch Indices.png",
-    path = file.path("figures"),
+    path = file_path,
     plot = p,
     width = 8,
     height = 6,
